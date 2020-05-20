45507c8d355cc7592011a23bfe64044c3b54cfed
Hiren Thacker <hithacker@gmail.com>
2020-05-20 10:14:55 +0530

    Update views to use address_level_type.level

--- create_views.sql
+++ create_views.sql
@@ -9,16 +9,20 @@ create or replace view jscs_lab_name_view as (select * from concept where id in 
 
 drop view if exists jscs_block_view;
 create or replace view jscs_block_view as (
-  select * from address_level where level = 3 and title in ('Anuppur','Kotma','Pushparajgarh','Jaithari')
+  select al.* from address_level al
+  join address_level_type alt on alt.id = al.type_id
+  where alt.level = 3 and al.title in ('Anuppur','Kotma','Pushparajgarh','Jaithari')
 );
 
 drop view if exists jscs_villages_view;
 create or replace view jscs_villages_view as (
   select villages.*,block.id blockid, block.title blocktitle from address_level villages
+  join address_level_type villages_type on villages_type.id = villages.type_id
   join location_location_mapping m2 on villages.id = m2.location_id
   join address_level block on block.id = m2.parent_location_id
-  where block.level = 3 and block.title in ('Anuppur','Kotma','Pushparajgarh','Jaithari')
-    and villages.level = 1
+  join address_level_type block_type on block_type.id = block.type_id
+  where block_type.level = 3 and block.title in ('Anuppur','Kotma','Pushparajgarh','Jaithari')
+    and villages_type.level = 1
 );
 
 set role none;
